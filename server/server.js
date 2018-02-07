5b45bc27e69206921e6203563a9990b020066e51
Zeydi <oussema.zeidi@ieee.org>
2018-02-07 11:29:20 +0100

    added delete tests

--- server/server.js
+++ server/server.js
@@ -46,13 +46,13 @@ app.get('/todos/:id', (req, res) => {
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
   if (!ObjectID.isValid(id)) {
-    return res.status(404);
+    return res.status(404).send();
   }
-  Todo.findByIdAndRemove(id).then((doc) => {
-    if (!doc) {
-      res.status(400).send();
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
     }
-    res.send({doc})
+    res.send({todo})
   }).catch((e) => {
     res.status(400).send();
   });
